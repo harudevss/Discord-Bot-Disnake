143cc431bd7982c81ed0bfeb8af67154a6cc77e2
Haru <chrmhrtsjacket@gmail.com>
2024-08-03 00:03:17 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+#Main
+botToken = "" #https://discord.com/developers/applications
+botPrefix = "."
+mainColor=0x36393F
---
+++ main.py
@@ -0,0 +1,5 @@
+from src import DiscordClient
+
+client = DiscordClient()
+
+client.startup()
